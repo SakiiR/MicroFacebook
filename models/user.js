f525db98e5a99ef382fb3f7072e19461fb068c88
Erwan Dupard <erwan.dupard@epitech.eu>
2016-09-15 14:13:33 +0200

    Adding email mathing

--- models/user.js
+++ models/user.js
@@ -6,7 +6,7 @@ var UserSchema = new mongoose.Schema({
   firstname : {type : String, required : true, unique : false},
   lastname  : {type : String, required : true, unique : false},
   username  : {type : String, required : true, unique : true, dropDups: true},
-  email     : {type : String, required : true, unique : true, dropDups: true},
+  email     : {type : String, required : true, unique : true, dropDups: true, match : [/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/, 'Please fill a valid email address']},
   password  : {type : String, required : true, unique : false}
 });
 
